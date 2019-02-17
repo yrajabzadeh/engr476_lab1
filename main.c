25d57abe4db7e6c6bd75dd6744ae23ed31d3a16d
Yousef Rajabzadeh <>
2019-02-17 01:39:11 -0800

    typedef struct added

--- main.c
+++ main.c
@@ -5,7 +5,33 @@
 
 #define class_size 7
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(void)
+{
+    printf("\n");
+    printf("Welcome to University!\n");
+    printf("\n");
+    printf("Main Menu");
+    printf("\n");
+
+    typedef struct Name classroom;
+    {
+        int number;
+        char namespace [50];
+        double subject_a;
+        double subject_b;
+    } students;
+    void sort (students studentInfo[], int i)
+    {
+        int x, y;
+
+        for (y = 1; y < i; y++) {
+            for (x = 0; x < i - y; x++) {
+                if (studentInfo[x].subject_a > studentInfo[x + 1].subject_a) {
+                    student temp = studentInfo[x];
+                    studentInfo[x] = studentInfo[x + 1];
+                    studentInfo[x + 1] = temp;
+                }
+            }
+        }
+    }
 }
